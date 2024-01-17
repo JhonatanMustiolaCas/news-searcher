d75c8a1fd6600cd118563add74cca1f1aa9f52e2
JhonatanMustiolaCas <jhonmustiomaths@protonmail.com>
2024-01-17 14:19:41 -0400

    initial

---
+++ src/hooks/useNoticias.jsx
@@ -0,0 +1,7 @@
+import { useContext } from "react"
+import NoticiasContext from "../context/NoticiasProvider";
+
+
+const useNoticias = () => useContext(NoticiasContext);
+
+export default useNoticias;
